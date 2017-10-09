a3189b331fb60a20a5b06325726596836dff4f8a
Kaushik <kaushik.konar@bsci.com>
2017-10-09 15:58:41 -0500

    updated to debug with echo

--- install-pcf/gcp/tasks/check-opsman-dns/task.sh
+++ install-pcf/gcp/tasks/check-opsman-dns/task.sh
@@ -3,6 +3,8 @@ set -e
 
 source "pcf-pipelines/functions/check_opsman_available.sh"
 
+echo Opsman variable is:  "${opsman_domain_or_ip_address}"
+
 opsman_available=$(check_opsman_available "${opsman_domain_or_ip_address}")
 if [[ $opsman_available != "available" ]]; then
   echo Could not reach opsman.${pcf_ert_domain}. Is DNS set up correctly?
