905bdf3755c2773998082721a81a435f6dda628c
Kaushik <kaushik.konar@bsci.com>
2017-10-09 12:14:36 -0500

    updating networks.tf with renamed sub networks

--- install-pcf/gcp/terraform/networks.tf
+++ install-pcf/gcp/terraform/networks.tf
@@ -4,28 +4,28 @@ resource "google_compute_network" "pcf-virt-net" {
 
 // Ops Manager & Jumpbox
 resource "google_compute_subnetwork" "subnet-ops-manager" {
-  name          = "infra-sub"
+  name          = "infra"
   ip_cidr_range = "${var.cidr_ops}"
   network       = "${google_compute_network.pcf-virt-net.self_link}"
 }
 
 // ERT
 resource "google_compute_subnetwork" "subnet-ert" {
-  name          = "ert-sub"
+  name          = "ert"
   ip_cidr_range = "${var.cidr_ert}"
   network       = "${google_compute_network.pcf-virt-net.self_link}"
 }
 
 // Services Tile
 resource "google_compute_subnetwork" "subnet-services-1" {
-  name          = "svc-sub"
+  name          = "svcs"
   ip_cidr_range = "${var.cidr_svc}"
   network       = "${google_compute_network.pcf-virt-net.self_link}"
 }
 
 // Dynamic Services Tile
 resource "google_compute_subnetwork" "subnet-dynamic-services-1" {
-  name          = "dynsvc-sub"
+  name          = "dyn-svcs"
   ip_cidr_range = "${var.cidr_dynsvc}"
   network       = "${google_compute_network.pcf-virt-net.self_link}"
 }
