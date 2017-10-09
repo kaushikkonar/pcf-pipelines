e8a631f52deb3a99fedb101e30053465e77d5a12
Kaushik <kaushik.konar@bsci.com>
2017-10-09 11:16:52 -0500

    updated networks.tf by updating names of the sub networks

--- install-pcf/gcp/terraform/networks.tf
+++ install-pcf/gcp/terraform/networks.tf
@@ -4,28 +4,28 @@ resource "google_compute_network" "pcf-virt-net" {
 
 // Ops Manager & Jumpbox
 resource "google_compute_subnetwork" "subnet-ops-manager" {
-  name          = "${var.prefix}-subnet-infrastructure-${var.gcp_region}"
+  name          = "infra-sub"
   ip_cidr_range = "${var.cidr_ops}"
   network       = "${google_compute_network.pcf-virt-net.self_link}"
 }
 
 // ERT
 resource "google_compute_subnetwork" "subnet-ert" {
-  name          = "${var.prefix}-subnet-ert-${var.gcp_region}"
+  name          = "ert-sub"
   ip_cidr_range = "${var.cidr_ert}"
   network       = "${google_compute_network.pcf-virt-net.self_link}"
 }
 
 // Services Tile
 resource "google_compute_subnetwork" "subnet-services-1" {
-  name          = "${var.prefix}-subnet-services-1-${var.gcp_region}"
+  name          = "svc-sub"
   ip_cidr_range = "${var.cidr_svc}"
   network       = "${google_compute_network.pcf-virt-net.self_link}"
 }
 
 // Dynamic Services Tile
 resource "google_compute_subnetwork" "subnet-dynamic-services-1" {
-  name          = "${var.prefix}-subnet-dynamic-services-1-${var.gcp_region}"
+  name          = "dynsvc-sub"
   ip_cidr_range = "${var.cidr_dynsvc}"
   network       = "${google_compute_network.pcf-virt-net.self_link}"
 }
